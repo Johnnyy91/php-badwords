26e1f15fa1c6a1b7a7812a390a98ae7c45483295
Johnnyy91 <claudiosardilli@gmail.com>
2022-10-12 18:27:41 +0200

    modification file

--- index.php
+++ index.php
@@ -3,7 +3,9 @@
 $Text='Hello world this is my first text for php';
 echo $Text . '<br>';
 echo strlen($Text) . '<br>';
+
 $VariableText=$_GET['changeWord'];
+
 $TextChange=str_replace($VariableText, 'XXX', $Text);
 echo $TextChange . '<br>';
 echo strlen($TextChange);
